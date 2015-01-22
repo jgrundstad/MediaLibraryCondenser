6086b68ceae5ae89a8dbfc383ef6e44a7d4a1c89
Jason Grundstad <jgrundstad@gmail.com>
2015-01-22 15:29:50 -0600

    refactoring

--- MediaLibraryCondenser.py
+++ MediaLibraryCondenser.py
@@ -3,19 +3,21 @@ import os
 import sys
 import sqlite3
 import glob
-from util import ParseConfig, create_db
+from util import ParseConfig, CreateDB
 
 class MediaLibraryCondenser():
 
 	def __init__(self, config_file):
 		print "running"
-
+    CDB = CreateDB(config_file)
 
 def main():
 	parser = argparse.ArgumentParser(
 		description = 'Find duplicate media files from a set of directories. ' )
 	parser.add_argument('-c', '--config', required=True, help='ini-style config',
 			                action='store', dest='config')
+  parser.add_argument('-d', '--createdb', help='create a new file database',
+                      action='store_true', dest='createdb')
 	args = parser.parse_args()
 
 	mlc = MediaLibraryCondenser(args.config)
