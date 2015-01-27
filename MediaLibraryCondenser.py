08b013c052d019807bfb21b171994a016fcd41c2
Jason Grundstad <jgrundstad@gmail.com>
2015-01-26 21:02:21 -0600

    parsing config file, starting directory walking

--- MediaLibraryCondenser.py
+++ MediaLibraryCondenser.py
@@ -1,26 +1,46 @@
 import argparse
+import glob
+import fnmatch
 import os
 import sys
 import sqlite3
-import glob
 from util import ParseConfig, CreateDB
 
 class MediaLibraryCondenser():
 
-	def __init__(self, config_file):
-		print "running"
-    CDB = CreateDB(config_file)
+  def __init__(self, config_file, createdb):
+    print "parsing config file"
+    pc = ParseConfig.ParseConfig(config_file)
+    self.config = pc.get_dict()
+
+    if createdb:
+      CDB = CreateDB.CreateDB(self.config)
+
+    self.walk_dirs()
+
+
+  def walk_dirs(self):
+    for dir in self.config['search'].split(' '):
+      print "Walking " + dir
+      for root, dirnames, filenames in os.walk(dir):
+        print "root: %s" % root
+        for filename in fnmatch.filter(filenames, '*.jpg'):
+          print os.path.join(root, filename)
+        for filename in fnmatch.filter(filenames, '*.JPG'):
+          print os.path.join(root, filename)
+
+
 
 def main():
-	parser = argparse.ArgumentParser(
-		description = 'Find duplicate media files from a set of directories. ' )
-	parser.add_argument('-c', '--config', required=True, help='ini-style config',
-			                action='store', dest='config')
+  parser = argparse.ArgumentParser(
+    description = 'Find duplicate media files from a set of directories. ' )
+  parser.add_argument('-c', '--config', required=True, help='ini-style config',
+                      action='store', dest='config')
   parser.add_argument('-d', '--createdb', help='create a new file database',
-                      action='store_true', dest='createdb')
-	args = parser.parse_args()
+                      action='store_true', dest='createdb', default=0)
+  args = parser.parse_args()
 
-	mlc = MediaLibraryCondenser(args.config)
+  mlc = MediaLibraryCondenser(args.config, args.createdb)
 
 if __name__ == '__main__':
-	main()
+  main()
--- util/ParseConfig.py
+++ util/ParseConfig.py
@@ -3,38 +3,40 @@ import sys
 
 class ParseConfig():
 
-	def __init__(self, config_file):
-		self.config_dict = dict()
-		self.config_file = config_file
-		self.config = ConfigParser.RawConfigParser(allow_no_value=True)
-		self.__read_config()
+  def __init__(self, config_file):
+    self.config_dict = dict()
+    self.config_file = config_file
+    self.config = ConfigParser.RawConfigParser(allow_no_value=True)
+    self.__read_config()
 
-	def __read_config(self):
-		try:
-			self.config.read(self.config_file)
-		except IOError:
-			print >>sys.stderr, 'ERROR: can\'t find config file: ' + self.config_file
-			sys.exit()
+  def __read_config(self):
+    try:
+      self.config.read(self.config_file)
+    except IOError:
+      print >>sys.stderr, 'ERROR: can\'t find config file: ' + self.config_file
+      sys.exit()
 
 
-	def print_dict(self):
-		for key in self.config.sections():
-			print key
-			for opt, val in self.config.items(key):
-				print "- " + opt + ', ' + val
-				self.config_dict[opt] = val
-				
+  def get_dict(self):
+    for key in self.config.sections():
+      print "[%s]" % key
+      for opt, val in self.config.items(key):
+        print >>sys.stderr, opt + ' = ' + val
+        self.config_dict[opt] = val
+    return self.config_dict
+        
 
-	def get(self):
-		return self.config_dict
+  #def get_dict(self):
+  #  return self.config_dict
 
 
 def main():
 
-	pc = ParseConfig(sys.argv[1])
-	d = pc.get()
-	for t in d:
-		print t + ' - ' + d[t]
+  pc = ParseConfig(sys.argv[1])
+  d = pc.get_dict()
+  for t in d:
+    print t + ' - ' + d[t]
+
 
 if __name__ == '__main__':
-	main()
+  main()
